a52856396b74d4c87b7f9b91c0525525f29dee8f
Sile <sinisa.buncic7@gmailc.om>
2018-08-19 16:56:36 +0200

    Modified database config

--- config/database.php
+++ config/database.php
@@ -41,17 +41,18 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'id5911802_project_ecommerce'),
+            'username' => env('DB_USERNAME', 'id5911802_ecommerce'),
+            'password' => env('DB_PASSWORD', 'supaodzelja94'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => false,
             'engine' => null,
+            'options'   => [PDO::ATTR_EMULATE_PREPARES => true,],
         ],
 
         'pgsql' => [
